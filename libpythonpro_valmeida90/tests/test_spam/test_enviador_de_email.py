f0eaff5184fa12162ae44dd8412444dee795aae0
viniciusalmeida <Victormariano1807>
2022-02-04 00:49:38 -0300

    Criado os primeiros testes automáticos

---
+++ libpythonpro_valmeida90/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,23 @@
+import pytest
+
+from libpythonpro_valmeida90.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['renzo@python.pro.br', 'foo@bar.com.br']
+)
+
+def test_remetente(destinatario):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        destinatario,
+        'luciano@python.pro.br',
+        'Cursos Python Pro',
+        'Primeira Turma Guido Von Rossum aberta'
+    )
+    assert destinatario in resultado
