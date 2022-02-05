eb4c67d5551d4d70c0d13384e7ecc5ee092eb429
viniciusalmeida <Victormariano1807>
2022-02-05 19:20:12 -0300

    Finalizado os testes de usuario.

--- libpythonpro_valmeida90/tests/test_spam/test_usuarios.py
+++ libpythonpro_valmeida90/tests/test_spam/test_usuarios.py
@@ -1,25 +1,34 @@
+import pytest
+
 from libpythonpro_valmeida90.spam.db import Conexao
 from libpythonpro_valmeida90.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.rool_back()
+    sessao_obj.fechar()
+
+
+@pytest.fixture(scope='session')
+def conexao():
+    # Setup
+    conexao_obj = Conexao()
+    yield Conexao()
+    # Tear Down
+    conexao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Renzo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.rool_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuario(sessao):
     usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.rool_back()
-    sessao.fechar()
-    conexao.fechar()
